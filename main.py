eec3ffcdd46779087afc98e26c5587679b2c89ac
Tommy <taguilu01@gmail.com>
2020-05-11 07:37:20 -0700

    first commit

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import requests
 # (or another user name)
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
+    user = input("Please specify User ")
+    try:
+        response = requests.get(f"https://api.github.com/users/{user}/events")
+        events = json.loads(response.content)
+        output = (events[0]['created_at'])
+        print(f'last commit from {user} at {output}')
+    except IndexError:
+        print("User not found")
     
 
 
